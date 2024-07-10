b6a0fc91877790ec9fb6e569905960ac8edba107
harshitha j <thejthanu@gmail.com>
2024-07-10 15:50:02 +0530

    changes

--- src/db/dbconnect.js
+++ src/db/dbconnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=> {
     try {
-        console.log("DB URL ", process.env.MONGODB_URI );
+        // console.log("DB URL ", process.env.MONGODB_URI );
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
         console.log("\nMongoDB Connected !! DB Host:", connectionInstance.connection.host);
         console.log("DB Name:", connectionInstance.connection.name);
